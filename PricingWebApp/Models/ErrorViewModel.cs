354853ec4032a3c574289fda6a2d84dbedca98f0
Anas Sadek <71427414+AnasSadek@users.noreply.github.com>
2024-01-01 16:46:29 +0100

    add file

---
+++ PricingWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PricingWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
