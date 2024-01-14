80e0f6e015fafdca9fc61242f255eef43da55a0b
saeed <saed.alskaf@gmail.com>
2024-01-14 18:18:45 +0300

    add employees page

--- PricingWebApp/Models/Employees.cs
+++ PricingWebApp/Models/Employees.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PricingWebApp.Models
 {
     public class Employees
     {
+        [Key]
         public int Id { get; set; }
         public string? First_Name { get; set; }
         public string? Last_Name { get; set; }
