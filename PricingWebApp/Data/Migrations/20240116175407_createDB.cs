31dca3bfbb2c9510b49bb23b94ef989b90cc4c16
Anas Sadek <asadek@invitec.com>
2024-01-17 10:50:15 +0100

    Remove unused Property

--- PricingWebApp/Data/Migrations/20240116175407_createDB.cs
+++ PricingWebApp/Data/Migrations/20240116175407_createDB.cs
@@ -33,10 +33,9 @@ namespace PricingWebApp.Data.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
                     Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MonthlyCost = table.Column<double>(type: "float", nullable: false),
                     LastUpdate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    MonthlyCost = table.Column<double>(type: "float", nullable: false)
                 },
                 constraints: table =>
                 {
