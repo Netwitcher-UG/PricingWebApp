439633bf485d6ceb81de20ed137a38d8ed70d945
brooj <kafi.brejawi@gmail.com>
2024-01-26 21:21:58 +0300

    latest changes

--- PricingWebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PricingWebApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,60 +1,61 @@
 ﻿
-#nullable disable
+    #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.UI.Services;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
 
-namespace PricingWebApp.Areas.Identity.Pages.Account
-{
-    public class LoginModel : PageModel
+    namespace PricingWebApp.Areas.Identity.Pages.Account
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly ILogger<LoginModel> _logger;
-
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        public class LoginModel : PageModel
         {
-            _signInManager = signInManager;
-            _logger = logger;
-        }
+            private readonly SignInManager<IdentityUser> _signInManager;
+            private readonly ILogger<LoginModel> _logger;
+
+            public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+            {
+                _signInManager = signInManager;
+                _logger = logger;
+            }
 
     
-        [BindProperty]
-        public InputModel Input { get; set; }
+            [BindProperty]
+            public InputModel Input { get; set; }
 
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+            public IList<AuthenticationScheme> ExternalLogins { get; set; }
 
-        public string ReturnUrl { get; set; }
+            public string ReturnUrl { get; set; }
 
-        [TempData]
-        public string ErrorMessage { get; set; }
+            [TempData]
+            public string ErrorMessage { get; set; }
 
        
-        public class InputModel
-        {
+            public class InputModel
+            {
        
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+                [Required]
+                [EmailAddress]
+                public string Email { get; set; }
 
         
-            [Required]
-            [DataType(DataType.Password)]
-            public string Password { get; set; }
+                [Required]
+                [DataType(DataType.Password)]
+                public string Password { get; set; }
 
         
-            [Display(Name = "Remember me?")]
-            public bool RememberMe { get; set; }
-        }
+                [Display(Name = "Remember me?")]
+                public bool RememberMe { get; set; }
+            }
+
 
         public async Task OnGetAsync(string returnUrl = null)
         {
@@ -65,6 +66,13 @@ namespace PricingWebApp.Areas.Identity.Pages.Account
 
             returnUrl ??= Url.Content("~/");
 
+            // Check if the user is already authenticated
+            if (User.Identity.IsAuthenticated)
+            {
+                // Redirect to the home page or any other page after successful login
+                returnUrl??= Url.Content ("/Logout");  // Adjust the path accordingly
+            }
+
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
@@ -73,39 +81,40 @@ namespace PricingWebApp.Areas.Identity.Pages.Account
             ReturnUrl = returnUrl;
         }
 
+
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {
-            returnUrl ??= Url.Content("~/");
-
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
-            if (ModelState.IsValid)
             {
-                
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
-                }
-            }
+                returnUrl ??= Url.Content("~/");
 
-            // If we got this far, something failed, redisplay form
-            return Page();
+                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+                if (ModelState.IsValid)
+                {
+                
+                    var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                    if (result.Succeeded)
+                    {
+                        _logger.LogInformation("User logged in.");
+                        return LocalRedirect(returnUrl);
+                    }
+                    if (result.RequiresTwoFactor)
+                    {
+                        return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                    }
+                    if (result.IsLockedOut)
+                    {
+                        _logger.LogWarning("User account locked out.");
+                        return RedirectToPage("./Lockout");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        return Page();
+                    }
+                }
+
+                // If we got this far, something failed, redisplay form
+                return Page();
+            }
         }
     }
-}
